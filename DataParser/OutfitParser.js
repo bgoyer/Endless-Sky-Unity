4f4ab0212c9853ccbe9729cd0fc16dd91f6e3b7f
Tim Goyer <tim.goyer@gmail.com>
2021-01-28 17:03:07 -0600

    Update parser.

--- DataParser/OutfitParser.js
+++ DataParser/OutfitParser.js
@@ -4,65 +4,68 @@ const uuid = require("uuid");
 ["engine", "harvesting", "outfit", "power", "weapon"].forEach(parser);
 
 function parser(fileName) {
-  var itemIdx = -1;
-  const items = [];
-  const data = fs.readFileSync(`./data/${fileName}.txt`, "UTF-8");
-  const lines = data
-    .split(/\r?\n/)
-    .filter((line) => {
-      return !line.startsWith("#") && line.trimLeft().length > 0;
-    })
-    .map((line) => {
-      return line.trim();
-    });
+    var itemIdx = -1;
+    const items = [];
+    const data = fs.readFileSync(`./data/${fileName}.txt`, "UTF-8");
 
-  lines.forEach((line) => {
-    var match = line.match(/"([^"]+)"/);
-    var hasQuotedName = match != null && match.index === 0;
+    data.split(/\r?\n/)
+        .filter((line) => {
+            return !line.startsWith("#") && line.trimLeft().length > 0;
+        })
+        .map((line) => {
+            return line.trim();
+        })
+        .forEach((line) => {
+            var match = line.match(/"([^"]+)"/);
+            var hasQuotedName = match != null && match.index === 0;
 
-    var rawName = hasQuotedName
-      ? match[1]
-      : line.substring(0, line.indexOf(" "));
+            var rawName = hasQuotedName
+                ? match[1]
+                : line.substring(0, line.indexOf(" "));
 
-    var name = rawName
-      .replace(
-        /\w\S*/g,
-        (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
-      )
-      .replace(" ", "");
+            var name = rawName
+                .replace(
+                    /\w\S*/g,
+                    (txt) =>
+                        txt.charAt(0).toUpperCase() +
+                        txt.substr(1).toLowerCase()
+                )
+                .replace(" ", "");
 
-    var value = line
-      .substring(hasQuotedName ? rawName.length + 2 : rawName.length)
-      .trim();
+            var value = line
+                .substring(hasQuotedName ? rawName.length + 2 : rawName.length)
+                .trim();
 
-    if (name == "Outfit" || name == "Effect") {
-      itemIdx++;
-      items[itemIdx] = {};
-      items[itemIdx] = {
-        ...items[itemIdx],
-        ID: uuid.v4(),
-        Type: name,
-      };
-      name = "Name";
-    }
-    var finalValue =
-      name === "Description" && items[itemIdx][name] != null
-        ? (items[itemIdx][name] += value)
-        : value;
+            if (name == "Outfit" || name == "Effect") {
+                itemIdx++;
+                items[itemIdx] = {};
+                items[itemIdx] = {
+                    ...items[itemIdx],
+                    ID: uuid.v4(),
+                    Type: name,
+                };
+                name = "Name";
+            }
 
-    items[itemIdx] = {
-      ...items[itemIdx],
-      [name]: finalValue.replace(/"/g, "").replace(/\t/g, " "),
-    };
+            var finalValue =
+                name === "Description" && items[itemIdx][name] != null
+                    ? (items[itemIdx][name] += value)
+                    : value;
+
+            items[itemIdx] = {
+                ...items[itemIdx],
+                [name]: finalValue.replace(/"/g, "").replace(/\t/g, " "),
+            };
+        });
+
     fs.writeFileSync(
-      `./data/output/${fileName}-en.json`,
-      JSON.stringify(
-        {
-          [`${fileName}Model`]: [...items],
-        },
-        null,
-        4
-      )
+        `./data/output/${fileName}-en.json`,
+        JSON.stringify(
+            {
+                [`${fileName}Model`]: [...items],
+            },
+            null,
+            4
+        )
     );
-  });
 }
